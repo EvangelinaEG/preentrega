8fadcd8a0f0eb36e553ba654d257460c3da372df
Evangelina <evangelina.gomes@ellecktra.com>
2024-08-15 00:19:07 -0300

    Se sgrega supertest

--- test/supertest.test.js
+++ test/supertest.test.js
@@ -19,8 +19,14 @@ describe('Teste de app', function() {
             console.log(statusCode);
             console.log(body);
         })
-        it('El endpoint /api/carts/:cart/products/:pid debe crear un producto dentro de un carrito', function(){
-                
+        it('El endpoint /api/carts/:cart/products/:pid debe crear un producto dentro de un carrito correctamente', async () => {
+                const cid = "66556550a2ad31a521163903"
+                const pid =  "664d1a31f798d6a20eba88d9"
+                const { statusCode, ok, body } = await resqueter.post(`/api/carts/${cid}/products/${pid}`);
+
+            expect(ok).to.be.equal(true);
+            expect(statusCode).to.be.equal(200);
+           
         } )
     })
 })
